1c917170d881339b8e69630970d0565d5ea06c8e
Симона Белчева <sb84517632@edu.mon.bg>
2022-02-28 17:33:31 +0200

    connector

---
+++ Game_Catalogue/Game_Catalogue/data/database.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.SqlClient;
+/// <summary>
+/// Microsoft SQL Server Database Connetion Class
+/// </summary>
+public static class DataBase
+{
+    private static string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=GAMECATALOGUE;Integrated Security=True;";
+
+    public static SqlConnection GetConnection()
+    {
+        return new SqlConnection(connectionString);
+    }
+}
