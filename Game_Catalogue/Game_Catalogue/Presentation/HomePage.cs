84063e75bf37d89b547a8ef0e32236f531f583d1
Ayhan <ayhan.hsn.01@gmail.com>
2022-03-06 09:29:08 +0200

    assigned material nugget to the main form :)))

--- Game_Catalogue/Game_Catalogue/Presentation/HomePage.cs
+++ Game_Catalogue/Game_Catalogue/Presentation/HomePage.cs
@@ -1,20 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using MaterialSkin.Controls;
 
 namespace Game_Catalogue.Presentation
 {
-    public partial class HomePage : Form
+    public partial class HomePage : MaterialForm
     {
+        readonly MaterialSkin.MaterialSkinManager materialSkinManager;
         public HomePage()
         {
             InitializeComponent();
+            materialSkinManager = MaterialSkin.MaterialSkinManager.Instance;
+            materialSkinManager.EnforceBackcolorOnAllComponents = true;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkin.MaterialSkinManager.Themes.DARK;
+            materialSkinManager.ColorScheme = new MaterialSkin.ColorScheme
+                (MaterialSkin.Primary.Purple500, MaterialSkin.Primary.Purple700,
+                MaterialSkin.Primary.Red100, MaterialSkin.Accent.Cyan700, MaterialSkin.TextShade.WHITE);
         }
     }
 }
