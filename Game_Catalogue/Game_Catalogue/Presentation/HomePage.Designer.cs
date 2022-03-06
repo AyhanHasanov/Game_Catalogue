b369dcbc4e2616ed61200e1b3892592f5b968c87
Ayhan <ayhan.hsn.01@gmail.com>
2022-03-06 10:40:30 +0200

    Changed homepages ui

--- Game_Catalogue/Game_Catalogue/Presentation/HomePage.Designer.cs
+++ Game_Catalogue/Game_Catalogue/Presentation/HomePage.Designer.cs
@@ -28,19 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.closeBox = new System.Windows.Forms.PictureBox();
+            this.MaximizeBox = new System.Windows.Forms.PictureBox();
+            this.minimizeBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.closeBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MaximizeBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minimizeBox)).BeginInit();
             this.SuspendLayout();
+            // 
+            // closeBox
+            // 
+            this.closeBox.Image = global::Game_Catalogue.Properties.Resources.icons8_close_52;
+            this.closeBox.Location = new System.Drawing.Point(1363, 12);
+            this.closeBox.Name = "closeBox";
+            this.closeBox.Size = new System.Drawing.Size(25, 25);
+            this.closeBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.closeBox.TabIndex = 0;
+            this.closeBox.TabStop = false;
+            this.closeBox.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // MaximizeBox
+            // 
+            this.MaximizeBox.Image = global::Game_Catalogue.Properties.Resources.icons8_maximize_button_52;
+            this.MaximizeBox.Location = new System.Drawing.Point(1332, 12);
+            this.MaximizeBox.Name = "MaximizeBox";
+            this.MaximizeBox.Size = new System.Drawing.Size(25, 25);
+            this.MaximizeBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.MaximizeBox.TabIndex = 0;
+            this.MaximizeBox.TabStop = false;
+            this.MaximizeBox.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // minimizeBox
+            // 
+            this.minimizeBox.Image = global::Game_Catalogue.Properties.Resources.icons8_subtract_52;
+            this.minimizeBox.Location = new System.Drawing.Point(1301, 12);
+            this.minimizeBox.Name = "minimizeBox";
+            this.minimizeBox.Size = new System.Drawing.Size(25, 25);
+            this.minimizeBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.minimizeBox.TabIndex = 0;
+            this.minimizeBox.TabStop = false;
+            this.minimizeBox.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // HomePage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1400, 700);
+            this.Controls.Add(this.minimizeBox);
+            this.Controls.Add(this.MaximizeBox);
+            this.Controls.Add(this.closeBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "HomePage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "HomePage";
+            ((System.ComponentModel.ISupportInitialize)(this.closeBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MaximizeBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minimizeBox)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private PictureBox closeBox;
+        private PictureBox MaximizeBox;
+        private PictureBox minimizeBox;
     }
 }
--- Game_Catalogue/Game_Catalogue/Presentation/HomePage.cs
+++ Game_Catalogue/Game_Catalogue/Presentation/HomePage.cs
@@ -1,23 +1,18 @@
 ﻿using Game_Data;
 using Game_Data.Model;
-using MaterialSkin.Controls;
 
 namespace Game_Catalogue.Presentation
 {
     /// <summary>
     /// UI
-    /// </summary>
-    /// <seealso cref="MaterialSkin.Controls.MaterialForm" />
-    public partial class HomePage : MaterialForm
+    /// </summary> />
+    public partial class HomePage : Form
     {
 
         /// <summary>
         /// Database Context
         /// </summary>
         private GameCatalogueContext games = new GameCatalogueContext();
-
-
-        readonly MaterialSkin.MaterialSkinManager materialSkinManager;
 
 
 
@@ -27,13 +22,11 @@ namespace Game_Catalogue.Presentation
         public HomePage()
         {
             InitializeComponent();
-            materialSkinManager = MaterialSkin.MaterialSkinManager.Instance;
-            materialSkinManager.EnforceBackcolorOnAllComponents = true;
-            materialSkinManager.AddFormToManage(this);
-            materialSkinManager.Theme = MaterialSkin.MaterialSkinManager.Themes.DARK;
-            materialSkinManager.ColorScheme = new MaterialSkin.ColorScheme
-                (MaterialSkin.Primary.Purple500, MaterialSkin.Primary.Purple700,
-                MaterialSkin.Primary.Red100, MaterialSkin.Accent.Cyan700, MaterialSkin.TextShade.WHITE);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
         }
     }
 }
